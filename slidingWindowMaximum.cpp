b24e3cf1f67eb329f4c6b2518a8d718e0c3fcca6
Procoder16 <smsoumik1608@gmail.com>
2021-06-14 11:57:58 +0530

    sliding window using deque added

--- slidingWindowMaximum.cpp
+++ slidingWindowMaximum.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<deque>
+#include<vector>
 using namespace std;
 
 int main(){
@@ -9,16 +11,43 @@ int main(){
     for(int i = 0; i<n; i++){
         cin>>arr[i];
     }
-                                                //THIS IS THE BRUTE FORCE APPROACH USING NORMAL ARRAY
     int k;
     cin>>k;
     
-    for(int i = 0; i<=n-k; i++){
-        int maxEle = arr[i];
-        for(int j = i; j <= i+2; j++){
-            maxEle = max(maxEle, arr[j]);
+    // for(int i = 0; i<=n-k; i++){
+    //     int maxEle = arr[i];
+    //     for(int j = i; j <= i+2; j++){          THIS IS THE BRUTE FORCE APPROACH USING NORMAL ARRAY
+    //         maxEle = max(maxEle, arr[j]);
+    //     }
+    //     cout<<maxEle<<" ";
+    // }
+
+    deque<int> q;
+    vector<int> ans;
+
+    for(int i = 0; i< k; i++){
+        while(!q.empty() && arr[q.back()] < arr[i]){
+            q.pop_back();
         }
-        cout<<maxEle<<" ";
+        q.push_back(i);
+    }
+    ans.push_back(arr[q.front()]);
+    
+    for(int i = k; i< n; i++){
+        if(q.front() == i - k){
+            q.pop_front();
+        }
+
+        while(!q.empty() && arr[q.back()] < arr[i]){
+            q.pop_back();
+        }
+        q.push_back(i);
+
+        ans.push_back(arr[q.front()]);
+    }
+
+    for(int i = 0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
     }
 
     return 0;
